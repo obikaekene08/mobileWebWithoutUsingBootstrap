61b54cd32eb1ad88dbde51b5e2905664f55c5721
ekene <obika.ekene@gmail.com>
2020-07-21 13:23:58 +0100

    firstCommit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+ <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Acme2 Photography</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <main>
+            <!--Landing Area-->
+            <div id="landing">
+                <div id="landing-text">
+                    <div id="landing-text-inner">
+                        <h1>Acme Photography</h1>
+                        <h2>Beautiful Natural Photography</h2>
+                        <a href="#images" class="btn" id = "view-work">View Work</a>
+                    </div>                    
+                </div>
+                <div id="landing-image">
+
+                </div>
+            </div>
+            <div id="images">
+                <div id="header">
+                    <h2>Our Work</h2>
+                </div>
+                <img src="https://source.unsplash.com/1600x900/?nature,water" alt="">
+                <div class="caption">
+                    <h3>Photo One</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, veritatis?</p>
+                </div>
+                <img src="https://source.unsplash.com/1600x900/?nature,flower" alt="">
+                <div class="caption">
+                    <h3>Photo Two</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, veritatis?</p>
+                </div>
+                <img src="https://source.unsplash.com/1600x900/?nature,animal" alt="">
+                <div class="caption">
+                    <h3>Photo Three</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, veritatis?</p>
+                </div>
+                <img src="https://source.unsplash.com/1600x900/?nature,tree" alt="">
+                <div class="caption">
+                    <h3>Photo Four</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, veritatis?</p>
+                </div>
+                <img src="https://source.unsplash.com/1600x900/?nature,clouds" alt="">
+                <div class="caption">
+                    <h3>Photo Five</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, veritatis?</p>
+                </div>
+
+            </div>
+        </main>
+
+        <footer>
+            <h3>Get In Touch</h3>
+            <p>Email or call us for a consult:</p>
+            <p>Email: <strong>contact@acme2photos.test</strong></p>
+            <p>Phone: <strong>(676) 555-555</strong></p>
+        </footer>
+
+    <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script>
+    </body>
+</html>
